5c76a2ed16c0094175dcc3283d324c1141d43569
BykaWF <yaroslavprozorov213@gmail.com>
2025-02-07 17:48:07 +0100

    Init remix project from scratch

---
+++ app/root.jsx
@@ -0,0 +1,26 @@
+import {
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+} from "@remix-run/dev"
+
+export default function App(){
+    return(
+        <html>
+            <head>
+                <link 
+                    rel="icon"
+                    href="data:image/x-icon;base64,AA"
+                />
+                <Meta/>
+                <Links/>
+            </head>
+            <body>
+                <h1>Hi remix</h1>
+                <Outlet/>
+                <Scripts/>
+            </body>
+        </html>
+    );
+}
